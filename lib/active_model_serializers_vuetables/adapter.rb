69b6b7e1ebd8c0e4b3bbe4c19814b3cb704665d7
Erlend Finvåg <erlend.finvag@gmail.com>
2018-01-20 00:29:08 +0100

    Only count relations

--- lib/active_model_serializers_vuetables/adapter.rb
+++ lib/active_model_serializers_vuetables/adapter.rb
@@ -2,13 +2,15 @@ module ActiveModelSerializersVuetables
   class Adapter < ActiveModelSerializers::Adapter::Json
 
     def serializable_hash(options = nil)
+      data = {}
       collection = serializer.object
 
       if collection.is_a? ActiveRecord::Relation
         collection = collection.unscope(:limit, :offset)
+        data[:count] = collection.count
       end
 
-      super.merge(count: collection.count)
+      super.merge(data)
     end
 
     def root
